5b07b87db7a1d3b1f4abc104cd577413b4281c23
lgrcia <lionel_garcia@live.fr>
2023-12-11 10:28:35 -0500

    fix: skip unused test

--- tests/test_jax_healpy.py
+++ tests/test_jax_healpy.py
@@ -5,6 +5,7 @@ import pytest
 from spotter import Star
 
 
+@pytest.mark.skip(reason="")
 @pytest.mark.parametrize("N", [2**n for n in range(1, 10)])
 @pytest.mark.parametrize(
     "center", [(0.5, 0.0), (np.pi / 2, 1.0), (1.0, np.pi), (1.0, 1.0)]
