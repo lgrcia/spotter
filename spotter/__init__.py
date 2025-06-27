04977206ebfaecb83013076b1d55a079ad564087
brackham <brackham@mit.edu>
2025-06-27 13:00:50 -0400

    :chore: added logic to expose `spotter.__version__`

--- spotter/__init__.py
+++ spotter/__init__.py
@@ -4,4 +4,11 @@ non-uniform stellar surfaces using HEALPix and JAX.
 
 """
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("spotter")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 from spotter.star import Star, show, video
