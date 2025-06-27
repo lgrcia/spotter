343a06a4b25e7eeeaa642447af27076d344b5ca5
lgrcia <lionel_garcia@live.fr>
2025-06-27 13:04:37 -0400

    fix: __version__ (#38)

--- spotter/__init__.py
+++ spotter/__init__.py
@@ -5,3 +5,9 @@ non-uniform stellar surfaces using HEALPix and JAX.
 """
 
 from spotter.star import Star, show, video
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("spotter")
+except PackageNotFoundError:
+    __version__ = "unknown"
